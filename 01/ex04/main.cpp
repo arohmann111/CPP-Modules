e2fad707b13e8b721a919ef218c3bf46851f75e5
arohmann <arohmann@student.42heilbronn.de>
2022-04-22 19:16:30 +0200

    220420221

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,24 +6,62 @@
 /*   By: arohmann <arohmann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 11:27:26 by arohmann          #+#    #+#             */
-/*   Updated: 2022/04/22 13:05:23 by arohmann         ###   ########.fr       */
+/*   Updated: 2022/04/22 19:16:01 by arohmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
-int my_replace(char **av)
+std::string my_replace(std::string search, std::string replace, std::string line)
 {
-    std::string filename = av[1];
-    std::string search = av[2];
-    std::string replace = av[3];
+	size_t pos = 0;
+	size_t l_search = search.length();
+	size_t l_replace = replace.length();
+	std::string newLine;
+	
+	pos = line.find(search);
+	while (pos != std::string::npos)
+	{
+		line.erase(pos, l_search);
+		line.insert(pos, replace);
+		pos = line.find(search, pos + l_replace);
+	}
+	return (line);
 }
 
 int main(int ac, char **av)
 {
-    if (ac != 4)
-        return (EXIT_FAILURE);
-    if (my_replace(av))
-        return(EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	std::string filename = av[1];
+	std::string search = av[2];
+	std::string replace = av[3];
+	std::string line;
+	std::ofstream ofs;
+	std::ifstream ifs;
+	
+	if (ac != 4)
+	{
+		std::cout << "Wrong Number of input => <filename> <search> <replace>" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	ifs.open(filename);
+	if (ifs.is_open())
+	{
+		ofs.open(filename.append(".replace"));
+		while(getline(ifs, line))
+		{
+			line = my_replace(search, replace, line);
+			ofs << line << std::endl;
+		}
+		ofs.close();
+	}
+	else
+	{
+		std::cout << "[ERROR] unable to open file" << std::endl;
+		ifs.close();
+		return (EXIT_FAILURE);
+	}
+	ifs.close();
+	return (EXIT_SUCCESS);
 }
