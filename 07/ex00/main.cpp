01146cbf46ce520e7bb618c404bcff0b31bff73d
arohmann <arohmann@student.42heilbronn.de>
2022-07-18 14:39:57 +0200

    180720222

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -2,22 +2,44 @@
 
 int main()
 {
-	int a = 10;
-	int b = 15;
+	std::cout << "\033[0;32mMY TEST\033[0;37m" << std::endl;
+	int x = 10;
+	int y = 15;
 
-	std::cout << max<int>(a, b) << std::endl;
+	std::cout << "max called with: " << x << " and " << y << std::endl;
+	std::cout << max<int>(x, y) << std::endl;
 
-	float c = 10.67f;
-	float d = 15.8f;
+	float v = 10.67f;
+	float w = 15.8f;
 
-	std::cout << max<float>(c, d) << std::endl;
+	std::cout << "max called with: " << v << " and " << w << std::endl;
+	std::cout << max<float>(v, w) << std::endl;
+
+	std::cout << "min called with: " << v << " and " << w << std::endl;
+	std::cout << min<float>(v, w) << std::endl;
 
 	char f = 'f';
 	char e = 'e';
 
 	swap<char>(f, e);
 
+	std::cout << "swap called with: " << f << " and " << e << std::endl;
 	std::cout << "f: " << f << " e: " << e << std::endl;
 
+
+	std::cout << "\033[0;32mORIGINAL TEST\033[0;37m" << std::endl;
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
