657598a1455db5506a03fc6e2a3500ebc87eeb7d
arohmann <arohmann@student.42heilbronn.de>
2022-07-21 13:13:31 +0200

    21072022

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -1,5 +1,42 @@
+#include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
-	
+	std::cout << "MutantStack" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3); mstack.push(5); mstack.push(737);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	MutantStack<int> s(mstack);
+
+	std::cout << "list" << std::endl;
+	std::list<int> lstack;
+	lstack.push_back(5);
+	lstack.push_back(17);
+	std::cout << lstack.back() << std::endl;
+	lstack.pop_back();
+	std::cout << lstack.size() << std::endl;
+	lstack.push_back(3); lstack.push_back(5); lstack.push_back(737);
+	std::list<int>::iterator lit = lstack.begin();
+	std::list<int>::iterator lite = lstack.end();
+	++lit;
+	--lit;
+	while (lit != lite) {
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+	std::list<int> ls(lstack);
+	return 0;
 }
